939b02696159eda8a6dbf1d2b8907d8925f4f8c2
Sahno007 <turnskin90@gmail.com>
2023-07-08 15:03:13 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  ingredientsList.appendChild(li);
+});
