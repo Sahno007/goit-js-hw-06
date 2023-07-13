9c00a74f80c504bfebd20c193573619f39db986e
Sahno007 <turnskin90@gmail.com>
2023-07-13 13:52:56 +0300

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -2,24 +2,43 @@ const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
+    width: '800px',
+    height: '530px',
   },
   {
     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    width: '800px',
+    height: '530px',
   },
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
+    width: '800px',
+    height: '530px',
   },
 ];
 
-const galleryList = document.querySelector(".gallery");
+const galleryList = document.querySelector('.gallery');
 
-const galleryItems = images
-  .map(
-    (image) =>
-      `<li class="gallery-item"><img src="${image.url}" alt="${image.alt}"></li>`
-  )
-  .join("");
+images.forEach((image) => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('gallery-item');
+ 
 
-galleryList.insertAdjacentHTML("beforeend", galleryItems);
+  const img = document.createElement('img');
+  img.src = image.url;
+  img.alt = image.alt;
+  img.style.width = image.width;
+  img.style.height = image.height;
+
+  listItem.appendChild(img);
+  galleryList.appendChild(listItem);
+});
+
+galleryList.style.display = 'flex';
+galleryList.style.listStyle = 'none';
+galleryList.style.margin = '0';
+galleryList.style.padding = '0';
+galleryList.style.justifyContent = 'space-between';
+galleryList.style.flexWrap = 'wrap';
